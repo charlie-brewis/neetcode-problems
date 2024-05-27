ddbd7bd05b9ab1ab08ba51872a87e4d72b06f177
Charlie Brewis <charliebrewis7@gmail.com>
2024-05-27 23:40:56 +0100

    validParentheses.c done

--- lessons/1-arrays/3-stacks/C/2-validParentheses.c
+++ lessons/1-arrays/3-stacks/C/2-validParentheses.c
@@ -28,7 +28,53 @@ Constraints:
     s consists of parentheses only '()[]{}'.
 */
 #include <stdbool.h>
+#include <stdio.h>
+
+#include <stdlib.h>
+#include <string.h>
 
 bool isValid(char* s) {
-    
+    // The number of items in s = memSizeOfs / memSizeOfs[0]
+    int sLength = strlen(s); 
+    // Define the stack to be of capcacity sLength so we wont have to do any resizing
+    char* stack = malloc(sizeof(char) * sLength);
+    int stackTopIndex = -1;
+
+    for (int i = 0; i < sLength; i++) {
+        char c = s[i];
+
+        // Push opening brackets
+        if (c == '(' || c == '[' || c == '{') {
+            stack[++stackTopIndex] = c;
+            continue;
+        }
+
+        // If the stack is currently empty, return false
+        if (stackTopIndex == -1) {
+            free(stack);
+            return false;
+        }
+
+        // If the top of the stack is not the corresponding opening bracket, the order is wrong, so invalid
+        char top = stack[stackTopIndex];
+        if (
+            c == ')' && top != '(' ||
+            c == ']' && top != '[' ||
+            c == '}' && top != '{' 
+            ) {
+                free(stack);
+                return false;
+        }
+        --stackTopIndex;
+    }
+    // If the stack is empty, all opening brackets have been paired, else there are unpaired opening brackets left
+    free(stack);
+    return stackTopIndex == -1;
+}
+
+
+
+int main() {
+    printf("out: %d", isValid("]"));
+    return 0;
 }
