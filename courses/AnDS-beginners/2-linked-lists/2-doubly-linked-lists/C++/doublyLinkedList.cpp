fbba864abf9c4ab81bffc6b7796cce170ea05693
Charlie Brewis <charliebrewis7@gmail.com>
2024-06-15 14:54:07 +0100

    Set up doubly linked list

---
+++ courses/AnDS-beginners/2-linked-lists/2-doubly-linked-lists/C++/doublyLinkedList.cpp
@@ -0,0 +1,9 @@
+/*
+Much like singly linked lists, a doubly linked list is comprised of multiple ListNode objects.
+Again, each ListNode object has a value and a pointer to the next node.
+However, where doubly linked lists differ is that they also have a pointer to the previous node.
+This allows for bidirectional traversal of the list, as well as simplifying the deletion logic.
+
+Where we decide to use a singly or doubly linked list will depend on the specific use case, and we 
+should consider how our linked list will be applied in our program.
+*/
