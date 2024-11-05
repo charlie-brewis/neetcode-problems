aa2dbf11d92361f6b82bd43c5c8746e141bd1d25
Charlie Brewis <charliebrewis7@gmail.com>
2024-11-05 17:45:02 +0000

    Added BSTDelete function

--- courses/AnDS-beginners/6-trees/2-binary-search-tree/C/binarySearchTree.c
+++ courses/AnDS-beginners/6-trees/2-binary-search-tree/C/binarySearchTree.c
@@ -47,16 +47,16 @@ void freeTree(TreeNode* root) {
 }
 
 
-int findMin(TreeNode* root) {
+TreeNode* findMin(TreeNode* root) {
     TreeNode* currentNode = root;
     while (currentNode && currentNode->left) { currentNode = currentNode->left; }
-    return currentNode->val;
+    return currentNode;
 }
 
-int findMax(TreeNode* root) {
+TreeNode* findMax(TreeNode* root) {
     TreeNode* currentNode = root;
     while (currentNode && currentNode->right) { currentNode = currentNode->right; }
-    return currentNode->val;
+    return currentNode;
 }
 
 
@@ -111,5 +111,54 @@ TreeNode* BSTInsert(TreeNode* root, int val) {
 
 /* == DELETION == */
 
-void BSTDelete(TreeNode* root, int val) {
+
+/*
+Complexity:
+    Time:
+        Best Case: O(log n)
+            - When node has 1 child
+            - Traverse to find node O(log n)
+
+        Average Case: O(log n)
+
+        Worst Case: O(log n)
+            - When node is root node
+            - Traverse to find node             O(1)
+            - Traverse to find max node         O(log n)
+            - Traverse again to delete max node O(log n)
+    
+    Space is also always O(log n)
+*/
+
+TreeNode* BSTDelete(TreeNode* root, int val) {
+/*
+    When deleting a TreeNode there are 2 cases:
+        Case 1: 0 or 1 child
+        Case 2: 2 children 
+*/
+    if (!root) return NULL; // Edge case
+
+    // Traverse until we find target node
+    if (val > root->val)      root->right = BSTDelete(root->right, val);
+    else if (val < root->val) root->left = BSTDelete(root->left, val);
+    
+    // Target Node found
+    else {
+        // Case 1: return the opposite side to the null side
+        if (root->left == NULL)       return root->right;
+        else if (root->right == NULL) return root->left;
+
+        // Case 2: Find the maximum node from the left subtree replace target's value with it.
+        //         - This ensures the tree remains ordered.
+        //         - Now delete the old node where this value was found - always case 1.
+        else {
+            TreeNode* maxNodeLeftSubtree = findMax(root->left);
+            root->val = maxNodeLeftSubtree->val;
+            root->left = BSTDelete(root->left, root->val);
+        }
+    }
+
+    return root;
 }
+// Note ive decided to find the maximum from the left subtree but this can also be done with the 
+// minimum from the right subtree.
