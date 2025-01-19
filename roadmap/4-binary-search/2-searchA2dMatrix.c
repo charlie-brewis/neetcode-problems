081f40cbff0f89bb810c7742e37eaf5a3b7b6660
Charlie Brewis <charliebrewis7@gmail.com>
2025-01-19 21:48:01 +0000

    Wrote solution

--- roadmap/4-binary-search/2-searchA2dMatrix.c
+++ roadmap/4-binary-search/2-searchA2dMatrix.c
@@ -36,6 +36,87 @@ Constraints:
 #include <stdbool.h>
 #include <stdio.h>
 
-bool searchMatrix(int** matrix, int matrixSize, int* matrixColSize, int target) {
+#define MAX_LENGTH 100
+
+int binarySearch(int* arr, int start, int end, int target) {
+    if (start >= end) return start;
+
+    int mid_i = (start + end) / 2;
+    int mid_val = arr[mid_i];
     
+    if (mid_val > target)      return binarySearch(arr, start, mid_i - 1, target);
+    else if (mid_val < target) return binarySearch(arr, mid_i + 1, end, target);
+    else return mid_i;
+}
+
+bool searchMatrix(int** matrix, int matrixSize, int* matrixColSize, int target) {
+    /*
+        Plan:
+            1. Define array of first column
+            2. Perform binary search on this array - modify binary search to return start in base case
+            3. We now have which row the target is in
+            4. Perform binary search on this row - return bool
+    */  
+    // 1. Define array of first column
+    int* firstCol = (int*) malloc(sizeof(int*) * MAX_LENGTH);
+    for (int i = 0; i < matrixSize; i++) {
+        firstCol[i] = matrix[i][0];
+    }
+
+    // 2. Perform binary search on this array
+    int row_i = binarySearch(firstCol, 0, matrixSize - 1, target);
+    printf("row_i = %d\n", row_i);
+
+    // 4. Perform binary search on this row
+    int target_i = binarySearch(matrix[row_i], 0, *matrixColSize, target);
+
+    return (matrix[row_i][target_i] == target);
+}
+
+
+
+int main() {
+    /*
+    Example 1:
+    Input: matrix = [
+        [1,3,5,7],
+        [10,11,16,20],
+        [23,30,34,60]
+        ], target = 3
+    Output: true
+    */
+   
+    int m = 3; // Num rows
+    int n = 4; // Num cols
+
+    // Allocate memory for matrix
+    int** matrix = (int**) malloc(sizeof(int*) * m);
+    for (int i = 0; i < n; i++) {
+        matrix[i] = (int*) malloc(sizeof(int) * n);
+    }
+
+    int values[3][4] = {
+        {1, 3, 5, 7},
+        {10, 11, 16, 20},
+        {23, 30, 34, 60}
+    };
+
+    // Populate Matrix
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            matrix[i][j] = values[i][j];
+        }
+    }
+
+    int matrixColSize = n;
+
+    printf("%d", searchMatrix(matrix, 3, &matrixColSize, 3));
+
+    // Free allocated memory
+    for (int i = 0; i < m; i++) {
+        free(matrix[i]);
+    }
+    free(matrix);
+
+    return 0;
 }
