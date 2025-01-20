dd24a10bd3990167c1b7e3a819bfe81275421be7
Charlie Brewis <charliebrewis7@gmail.com>
2025-01-20 22:55:48 +0000

    First working version

--- roadmap/1-arrays-and-hashing/7-productExceptSelf.c
+++ roadmap/1-arrays-and-hashing/7-productExceptSelf.c
@@ -17,7 +17,7 @@ Example 2:
  
 
 Constraints:
-    - 2 <= nums.length <= 105
+    - 2 <= nums.length <= 10^5
     - -30 <= nums[i] <= 30
     - The input is generated such that answer[i] is guaranteed to fit in a 32-bit integer.
 
@@ -25,16 +25,54 @@ Constraints:
 #include <stdlib.h>
 #include <stdio.h>
 
+#define MAX_LENGTH 100000
+
+
+int productOfSubArr(int* arr, int start, int end) {
+    // Edge cases
+    if (start > end) return 1;  // For last item
+    if (start == end) return arr[start]; // For second-last item
+
+    // Calculate the product between the ranges
+    int ans = arr[start];
+    for (int i = start + 1; i <= end; i++) ans *= arr[i];
+    return ans;
+}
 
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
+// O(n^2)
 int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
-    
+    // Define return values
+    *returnSize = numsSize;
+    int* returnArr = (int*) malloc(sizeof(int) * *returnSize);
+
+    // Sequentially calculate prefixProduct * suffixProduct
+    for (int i = 0; i < numsSize; i++) {
+        int prefixProduct = productOfSubArr(nums, 0, i - 1);
+        int suffixProduct = productOfSubArr(nums, i + 1, numsSize - 1);
+        printf("i: %d, prefix: %d, suffix: %d\n", i, prefixProduct, suffixProduct);
+
+        returnArr[i] = prefixProduct * suffixProduct;
+    }
+
+    return returnArr;
 }
 
 
 int main () {
+
+    int myArr[4] = {1, 2, 3, 4};
+    int returnSize = 0;
+
+    int* res = productExceptSelf(myArr, 4, &returnSize);
+
+    for (int i = 0; i < returnSize; i++) {
+        printf("%d, ", res[i]);
+    }
+
+    free(res);
 
     return 0;
 }
